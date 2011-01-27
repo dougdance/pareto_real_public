d7e5766c7eb30ab0781dddc4b68932c13c5bc8c6
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2011-01-27 21:44:28 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParetoRealPublic::Application.config.secret_token = '90d3d0680a94b5b883224228eb895f9ac4aecd34c870c547f1ee7886424f7542278dc97d198de6ff380a0f856c05d6aa2f6c8328a7f2d127f196847b8c01c644'
